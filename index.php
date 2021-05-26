a653c63ec8174589263605051f13937a5ffbcfd7
Phpshka <56721354+Rustam-LaraCast@users.noreply.github.com>
2021-05-26 16:36:01 +0600

    Telegram send

---
+++ index.php
@@ -0,0 +1,50 @@
+ <div class="form">
+          <div class="form__wrapper">
+<form id="form-contact" method="POST" class="form-element" autocomplete="off">
+     <div class="form-message" style="font-size:25px; margin-left:30%;"></div>
+            <div class="form-row">
+              <div class="form-group col-md-6">
+             <input type="text" name="name" class="form-control is-valid" id="validationServer01" placeholder="Ваше имя..." value="" required/>  
+              </div>
+              <div class="form-group col-md-6">
+                   
+					    <input style="background-color:white;" class="form-control is-valid" id="validationServer01" type="tel"   placeholder="Ваш номер..."  name="phone" required >
+                  
+              </div>
+            </div>
+            <div class="text-center"><button type="submit" class="btn btn-" style="background-color:#18d26e; color:white; margin-top:10px;">Отправить заявку</button></div>
+          </form>
+        </div>
+          </div>
+
+      
+           
+     <a href="#" class="back-to-top" style="left:15px;"><i class="fa fa-chevron-up"></i></a>
+    
+    
+    
+         
+
+  <!-- JavaScript Libraries -->
+  <script src="lib/jquery/jquery.min.js"></script>
+  <script src="lib/jquery/jquery-migrate.min.js"></script>
+  <script src="lib/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="lib/easing/easing.min.js"></script>
+  <script src="lib/superfish/hoverIntent.js"></script>
+  <script src="lib/superfish/superfish.min.js"></script>
+  <script src="lib/wow/wow.min.js"></script>
+  <script src="lib/waypoints/waypoints.min.js"></script>
+  <script src="lib/counterup/counterup.min.js"></script>
+  <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+  <script src="lib/isotope/isotope.pkgd.min.js"></script>
+  <script src="lib/lightbox/js/lightbox.min.js"></script>
+  <script src="lib/touchSwipe/jquery.touchSwipe.min.js"></script>
+  <!-- Contact Form JavaScript File -->
+  <script src="contactform/contactform.js"></script>
+
+  <!-- Template Main Javascript File -->
+  <script src="js/main.js"></script>
+    
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+    <script src="./telegramform/js/telegramform.js"></script>
+
